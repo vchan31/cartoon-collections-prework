d4132c5917db4ae222f9a897a996730041c0080a
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 20:37:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+def roll_call_dwarves(array1)
+
+  counter = 0
+  array1.each do |element|
+    counter += 1
+    puts "#{counter}. #{element}"
+    end
+  
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(array1)
+   array1.collect do |element|
+    element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array1)
+  array1.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+def find_the_cheese(array1)
+  array1.find do |word|
+  word === 'cheddar'|| word === "gouda" || word == "thyme"
+  end
 end
