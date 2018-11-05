695beaac6b22ae997ef0394459b3a316b2a5cb76
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 20:19:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,13 +19,12 @@ def summon_captain_planet(array1)
 end
 
 
-def long_planeteer_calls# code an argument here
-  def calls(array1)
+def long_planeteer_calls(array1)
   array1.any? do |word|
     word.length > 4
   end
 end
-end
+
 
 
 def find_the_cheese# code an argument here
